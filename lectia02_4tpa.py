e86cfd6f8b3222646c25eff95e05e0c048fe3885
Teodor <teodor.petrica@rovacc.ro>
2025-05-27 21:31:36 +0300

    add lectia 3

--- lectia02_4tpa.py
+++ lectia02_4tpa.py
@@ -10,3 +10,16 @@
 # Daca utilizatorul alege carnivor, sa se afiseze "Ai ales un meniu pe baza de carne."
 
 # Daca utilizatorul introduce altceva, sa se afiseze "Meniu invalid."
+
+print ("Alege un meniu: \n1 - vegetarian\n2 - vegan\n3 - carnivor")
+
+alegere :int = int(input("Introdu numarul optiunii: "))
+
+if alegere == 1:
+    print("Ai ales un meniu fara carne.")
+elif alegere == 2:
+    print("Ai ales un meniu fara produse de origine animala.")
+elif alegere == 3:
+    print("Ai ales un meniu pe baza de carne.")
+else:
+    print("Meniu invalid.")
---
+++ lectia03_1.py
@@ -0,0 +1,21 @@
+# Bucla repetitiva // loop
+
+# for   ->  cunosc numarul de iteratii/repetari
+# while ->  nu cunosc numarul de executii
+print("Exercitiu for: ")
+for numar in range(1,10):
+    print(numar)
+
+print("\nExercitiu while: ")
+nr = 1
+while nr <= 9:
+    print(nr)
+    nr += 1
+
+print("\nExercitiu for cu pas: ")
+# numerele pare <= 10
+for numar in range(0, 12, 2):
+    print(numar)
+
+print("\nExercitiu afisare numere de la 10 la 1: ")
+
